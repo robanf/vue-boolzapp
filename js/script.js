6b097c93e3540239354a529b8da6801f02d7f8aa
Roberto <robertoanfuso98@gmail.com>
2021-07-01 17:10:18 +0200

    add visualizzato

--- js/script.js
+++ js/script.js
@@ -16,18 +16,21 @@ const app= new Vue(
                             text: 'Hai portato a spasso il cane?',
                             status: 'sent',
                             open:false,
+                                color:true,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Ricordati di dargli da mangiare',
                             status: 'sent',
                             open:false,
+                                color:true,
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             text: 'Tutto fatto!',
                             status: 'received',
                             open:false,
+                                color:true,
                         }
                         ],
                     },
@@ -42,18 +45,21 @@ const app= new Vue(
                                 text: 'Ciao come stai?',
                                 status: 'sent',
                                 open:false,
+                                color:true,
                             },
                             {
                                 date: '20/03/2020 16:30:55',
                                 text: 'Bene grazie! Stasera ci vediamo?',
                                 status: 'received',
                                 open:false,
+                                color:true,
                             },
                             {
                                 date: '20/03/2020 16:35:00',
                                 text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                                 status: 'sent',
                                 open:false,
+                                color:true,
                             }
                         ],
                     },
@@ -68,18 +74,21 @@ const app= new Vue(
                                 text: 'La Marianna va in campagna',
                                 status: 'received',
                                 open:false,
+                                color:true,
                             },
                             {
                                 date: '28/03/2020 10:20:10',
                                 text: 'Sicuro di non aver sbagliato chat?',
                                 status: 'sent',
                                 open:false,
+                                color:true,
                             },
                             {
                                 date: '28/03/2020 16:15:22',
                                 text: 'Ah scusa!',
                                 status: 'received',
                                 open:false,
+                                color:true,
                             }
                         ],
                     },
@@ -94,12 +103,14 @@ const app= new Vue(
                                 text: 'Lo sai che ha aperto una nuova pizzeria?',
                                 status: 'sent',
                                 open:false,
+                                color:true,
                             },
                             {
                                 date: '10/01/2020 15:50:00',
                                 text: 'Si, ma preferirei andare al cinema',
                                 status: 'received',
                                 open:false,
+                                color:true,
                             }
                         ],
                     },
@@ -123,20 +134,22 @@ const app= new Vue(
                         date: date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+"  "+ date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(),
                         text: this.newmess,
                         status: 'sent',
-                        open:false
+                        open:false,
+                        color:false
                     
                 });
 
                 document.getElementById("accesso").innerHTML="sta scrivendo..."
                 setTimeout(function(){
-                    document.getElementsByClassName("test").style.color = "blue";
+                    app.contacts[app.count].messages[app.contacts[app.count].messages.length-1].color=true;
                 },1000)
                 setTimeout(function(){  
                     app.contacts[app.count].messages.push({
                     date: date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+"  "+ date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(),
                     text: app.risposte[Math.round(Math.random() * 4)],
                     status: 'received',
-                    open:false
+                    open:false,
+                    color:true
                 
                 });
                 document.getElementById("accesso").innerHTML="ultimo accesso:"+date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+"  "+ date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
